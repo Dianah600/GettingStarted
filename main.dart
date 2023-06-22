6f67964a8079c883a476e3c4c5c3fa2a34deb748
Dianah600 <137384693+Dianah600@users.noreply.github.com>
2023-06-22 10:47:01 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'body.dart';
+
+void main() {
+  runApp(HelloWorld());
+}
+
+class HelloWorld extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("CSC DAY CLASS"),
+          backgroundColor: Colors.red,
+        ),
+        body: Body(),
+      ),
+    );
+  }
+}
